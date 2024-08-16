b69be1c6c5138a2408be7615e64215f0614e705c
gaurimadan <gaurimadan12@gmail.com>
2024-08-17 00:47:00 +0530

    mailto added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="gauri.madaan.12@gmail.com">
+        <a href="mailto:gauri.madaan.12@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
